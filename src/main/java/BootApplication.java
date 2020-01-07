d2131692a0bcb8d2ec33f65fdbaa35304545d8f7
ZXJ <674919421@qq.com>
2020-01-07 20:42:31 +0800

    项目经理第一次提交boot

---
+++ src/main/java/BootApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.zxj.controller"})
+public class BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zxj/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.zxj.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/info")
+    @ResponseBody
+    public String info(){
+        return "Hello SpringBoot Info";
+    }
+
+}
